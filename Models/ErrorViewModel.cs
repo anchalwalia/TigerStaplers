772d9b15565ee60baa90f64597875036802f2b0c
anchalwalia <105453237+anchalwalia@users.noreply.github.com>
2022-10-14 11:26:27 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TigerStaplers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
