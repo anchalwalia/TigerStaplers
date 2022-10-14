f941c54f5b0c3e78b26a91b42a790989089ce73a
anchalwalia <105453237+anchalwalia@users.noreply.github.com>
2022-10-14 11:49:33 -0400

    added stapler controller

---
+++ Controllers/StaplersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TigerStaplers.Controllers
+{
+    public class StaplersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
