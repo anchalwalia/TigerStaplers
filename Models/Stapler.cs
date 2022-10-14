2512defcffa59226178e42e6c326f54ddb3b419a
anchalwalia <105453237+anchalwalia@users.noreply.github.com>
2022-10-14 12:56:14 -0400

    Incuded staplers.cs

---
+++ Models/Stapler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace TigerStaplers.Models
+{
+    public class Stapler
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+
+        public string Material { get; set; }
+
+        public decimal Price { get; set; }
+        public int PinsIncluded { get; set; }
+    }
+}
