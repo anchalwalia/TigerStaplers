ede929b36b4120c3fe5b6f18c8ef41746b12f180
anchalwalia <105453237+anchalwalia@users.noreply.github.com>
2022-10-16 19:56:03 -0400

    Included new file of view model.

---
+++ Models/StaplerSizeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace TigerStaplers.Models
+{
+    public class StaplerSizeViewModel
+    {
+        public List<Stapler> Staplers { get; set; }
+        public SelectList Size { get; set; }
+        public string StaplerSize { get; set; }
+        public string SearchString { get; set; }
+    }
+}
