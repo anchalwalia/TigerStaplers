0072b804e80873a7a24dce1b16c45969f6aae7ad
anchalwalia <105453237+anchalwalia@users.noreply.github.com>
2022-10-16 19:55:08 -0400

    Added datatype validation in staplers.cs.

--- Models/Stapler.cs
+++ Models/Stapler.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 namespace TigerStaplers.Models
@@ -7,13 +9,23 @@ namespace TigerStaplers.Models
     public class Stapler
     {
         public int Id { get; set; }
+
+        [StringLength(40, MinimumLength = 3)]
+        [Required]
         public string Name { get; set; }
         public string Color { get; set; }
         public string Size { get; set; }
 
         public string Material { get; set; }
 
+        [Range(1, 100)]
+        [Column(TypeName = "decimal(18, 2)")]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
         public int PinsIncluded { get; set; }
+
+        [Range(0, 5)]
+        [Column(TypeName = "decimal(2, 1)")]
+        public decimal Rating { get; set; }
     }
 }
